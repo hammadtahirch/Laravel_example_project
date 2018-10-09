aef50ba3fd3cf943f17adb62a85aebf388a553bc
Hammad Tahir <hammad.tahir@boldcommerce.com>
2018-10-09 14:31:59 -0500

    production build

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_78ffdd408839ce1'),
+            'username' => env('DB_USERNAME', 'b0911151f3fa3d'),
+            'password' => env('DB_PASSWORD', 'e3068533'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
